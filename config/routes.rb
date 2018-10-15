7a01b54eaed153458dfd5925d2fc53b821ebd136
kazuya-hachisuka <kzy8sk@gmail.com>
2018-10-15 05:50:53 +0000

    modify routes.rb, application.html.erb, books/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books
-	root 'books#index'
+  resources :reviews, only: [:index, :destroy]
+  root 'books#index'
+  resources :books do
+  	resources :reviews, except: [:index, :destroy]
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
